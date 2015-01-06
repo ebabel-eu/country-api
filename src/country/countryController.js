e3632d75ca3365cb5dc8d584810b2783ecd6ee92
Nadjib Amar <hello@ebabel.eu>
2015-01-06 16:00:08 +0100

    refactoring

--- src/country/countryController.js
+++ src/country/countryController.js
@@ -4,9 +4,33 @@ module.exports = function (app, options) {
 	var mongoose = options.mongoose,
 	    Schema = options.mongoose.Schema,
 	    db = options.db,
-	    countryGet = require('./countryGet'),
-	    countryModel = require('./countryModel')(db);
+        countryModel = require('./countryModel')(db);
 
 	// Get a list of all countries.
-	app.get('/country', countryGet);
+	app.get('/country', function (req, res) {
+        // Mongoose querying via querystring. 
+        // Ex: append ?limit=2 or ?filter={"id": "de"}
+        var qSkip = req.query.skip,
+            qLimit = req.query.limit,
+            qSort = req.query.sort,
+            qFilter = req.query.filter ? JSON.parse(req.query.filter) : {};
+
+        // Filter the result by any provided querystring parameters.
+        countryModel.find(qFilter)
+            .sort(qSort)
+            .skip(qSkip)
+            .limit(qLimit)
+            .exec(function (err, country) {
+                if (err) {
+                    // Return an error message if a valid response couldn't be formulated.
+                    return options.handleError(err, req, res, 'Could not list the records.');
+                }
+
+                // Return a valid response.
+                res.send({
+                    records: country.length,
+                    country: country
+                });
+            });
+    });
 };
